2e8c3b03b74ad4c3fd0049b024c90a40857532b2
PPetrusevski <pavel.petrusevski@gmail.com>
2021-01-23 23:07:00 +0100

    Added button animation and responsive menu overlay

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function () {
+  //RESPONSIVE MENU OVERLAY
 
+  $(".navbar-toggler-custom").click(function () {
+    $("body").addClass("overflow");
+    $(".target-overlay").addClass("overlay");
+    $(".nav-item a").addClass("text-white");
+    $(".navbar-brand").addClass("invisible");
+    $("nav").removeClass("nav-shadow");
+  });
+  $(".fa-times").click(function () {
+    $("body").removeClass("overflow");
+    $(".target-overlay").removeClass("overlay");
+    $(".nav-item a").removeClass("text-white");
+    $(".navbar-brand").removeClass("invisible");
+    $("nav").addClass("nav-shadow");
+  });
+});
