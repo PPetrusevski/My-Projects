6e34264403330791f185f6dfddd25707596e6b50
PPetrusevski <pavel.petrusevski@gmail.com>
2021-01-24 09:38:52 +0100

    Added card filter functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
+  //FILTERS FUNCTIONALITY
+
+  $(".list").click(function () {
+    let value = $(this).attr("data-filter");
+
+    if ($(this).hasClass("bg-active")) {
+      $(".itemBox").show("1300");
+    } else {
+      $(".itemBox")
+        .not("." + value)
+        .hide("1300");
+      $(".itemBox")
+        .filter("." + value)
+        .show("1300");
+    }
+  });
   //RESPONSIVE MENU OVERLAY
 
   $(".navbar-toggler-custom").click(function () {
@@ -14,5 +30,28 @@ $(document).ready(function () {
     $(".nav-item a").removeClass("text-white");
     $(".navbar-brand").removeClass("invisible");
     $("nav").addClass("nav-shadow");
+  });
+
+  //FILTER BUTTONS VISUAL
+
+  $(".list").click(function () {
+    $(this).toggleClass("bg-active").siblings().removeClass("bg-active");
+    if ($("#filter-1").hasClass("bg-active")) {
+      $("#filter-1").children("p").removeClass("text-white");
+    } else if ($("#filter-1").not("bg-active")) {
+      $("#filter-1").children("p").addClass("text-white");
+    }
+
+    if ($("#filter-2").hasClass("bg-active")) {
+      $("#filter-2").children("p").removeClass("text-white");
+    } else if ($("#filter-2").not("bg-active")) {
+      $("#filter-2").children("p").addClass("text-white");
+    }
+
+    if ($("#filter-3").hasClass("bg-active")) {
+      $("#filter-3").children("p").removeClass("text-white");
+    } else if ($("#filter-3").not("bg-active")) {
+      $("#filter-3").children("p").addClass("text-white");
+    }
   });
 });
