763bfd8ded8c308d9818f729ac192a8cc2423549
PPetrusevski <pavel.petrusevski@gmail.com>
2021-05-18 08:40:29 +0200

    Acceptable initial design attempt

---
+++ main.js
@@ -0,0 +1,41 @@
+$(function () {
+	const container = $(".elements-container");
+	const lanthanides = $(".lanthanides");
+	const actinides = $(".actinides");
+	$(Elements).each((_, el) => {
+		const xPosition = el.xpos == "down" || el.xpos == "sdown" ? "auto" : el.xpos;
+		const yPosition = el.ypos;
+		function createElementCard() {
+			return `
+      <div class="element ${el.name} ${
+				el.category
+			}" style="grid-column-start: ${xPosition}; grid-row-start: ${yPosition};">
+        <h3 class="symbol">${el.symbol}</h3>
+        <p class="fullElementName">${el.name}</p>
+        <span class="atomicNumber">${el.number ? el.number : ""}</span>
+        <ul class = "shellsGroup">
+          
+        </ul>
+      </div>
+      `;
+		}
+		function arrangeShells() {
+			for (let i = 0; i < el.shells.length; i++) {
+				$(`.${el.name} .shellsGroup`).append(`<li>${el.shells[i]}</li>`);
+			}
+		}
+		if (el.number >= 57 && el.number <= 71) {
+			lanthanides.append(createElementCard());
+			arrangeShells();
+		} else if (el.number >= 89 && el.number <= 103) {
+			actinides.append(createElementCard());
+			arrangeShells();
+		} else {
+			container.append(createElementCard());
+			arrangeShells();
+		}
+	});
+	// $(".element").on("click", function () {
+	// 	console.log("clicked");
+	// });
+});
