56f99f4ad11f84a88b7220a1d90188d3230e8350
Pavel Petrusevski <pavel.petrusevski@gmail.com>
2021-09-11 19:46:15 +0200

    Modal mods

--- src/Components/Controls/Modal.js
+++ src/Components/Controls/Modal.js
@@ -8,40 +8,5 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import Controls from "./Controls";
 
 export default function Modal({ children }) {
-	const [open, setOpen] = useState(false);
-
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
-
-	const handleClose = () => {
-		setOpen(false);
-	};
-
-	return (
-		<div>
-			<Button variant="outlined" color="primary" onClick={handleClickOpen}>
-				Open alert dialog
-			</Button>
-			<Dialog
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="alert-dialog-title"
-				aria-describedby="alert-dialog-description"
-			>
-				<DialogContent style={{ width: "80vw" }}>{children}</DialogContent>
-				<DialogActions>
-					<Controls.Button
-						size="small"
-						variant="text"
-						text="cancel"
-						onClick={handleClose}
-						color="primary"
-					/>
-
-					<Controls.Button size="small" text="add" onClick={handleClose} color="primary" />
-				</DialogActions>
-			</Dialog>
-		</div>
-	);
+	return { children };
 }
--- src/Pages/MainPages/Statistics.js
+++ src/Pages/MainPages/Statistics.js
@@ -1,14 +1,37 @@
-import { Grid, MenuItem, Select } from "@material-ui/core";
-import React from "react";
+import {
+	Dialog,
+	DialogActions,
+	DialogContent,
+	FormControl,
+	Grid,
+	MenuItem,
+	Select,
+} from "@material-ui/core";
+import React, { useState } from "react";
 import Controls from "../../Components/Controls/Controls";
 
 export default function Statistics() {
+	const [open, setOpen] = useState(true);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
 	return (
 		<Grid container>
 			<Grid item xs={10}>
-				<Controls.Modal>
-					<Grid container justifyContent="center">
-						<Grid item xs={10}>
+				<Dialog
+					open={open}
+					onClose={handleClose}
+					handleClose={handleClose}
+					aria-labelledby="alert-dialog-title"
+					aria-describedby="alert-dialog-description"
+				>
+					<DialogContent style={{ width: "80vw" }}>
+						<FormControl fullWidth>
 							<Select
 								labelId="demo-simple-select-label"
 								id="demo-simple-select"
@@ -21,9 +44,20 @@ export default function Statistics() {
 								<MenuItem value={20}>Twenty</MenuItem>
 								<MenuItem value={30}>Thirty</MenuItem>
 							</Select>
-						</Grid>
-					</Grid>
-				</Controls.Modal>
+						</FormControl>
+					</DialogContent>
+					<DialogActions>
+						<Controls.Button
+							size="small"
+							variant="text"
+							text="cancel"
+							onClick={handleClose}
+							color="primary"
+						/>
+
+						<Controls.Button size="small" text="add" onClick={handleClose} color="primary" />
+					</DialogActions>
+				</Dialog>
 			</Grid>
 		</Grid>
 	);
